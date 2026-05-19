a12d08a6f060e21878b7a12c709f72421c206306
jinsei <144908986+makino-yuto@users.noreply.github.com>
2026-05-19 23:35:13 +0900

    Refactor boundary conditions in ABC457/B.py

--- ABC457/B.py
+++ ABC457/B.py
@@ -6,16 +6,19 @@ for i in range(h):
     for j in range(w):
       if h == 1 and w == 1:
         A[i][j] = 0
+          
       elif h == 1:
         if j in [0, w - 1]:
           A[i][j] = 1
         else:
           A[i][j] = 2
+            
       elif w == 1:
         if i in [0, h - 1]:
           A[i][j] = 1
         else:
           A[i][j] = 2
+            
       elif i in [0, h - 1] and j in [0, w - 1]:
         A[i][j] = 2
       elif (i in [0, h - 1]) ^ (j in [0, w - 1]):
