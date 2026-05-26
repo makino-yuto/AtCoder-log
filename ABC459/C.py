5605ab1407e77a0d3e1a9370cd51dce7f27de8ca
makino-yuto <yuuto01222005@gmail.com>
2026-05-26 15:55:33 +0900

    update

--- ABC459/C.py
+++ ABC459/C.py
@@ -1,2 +1,20 @@
+n = 600000
+
 N, Q = map(int, input().split())
 
+a = [0] * (n + 1) #第nマスの高さ
+b = [0] * (n + 1) #ある高さnに何回積まれたか(わかりづらい)
+b[0] = N
+
+diff = 0
+
+for i in range(Q):
+    x, y = map(int, input().split())
+    if x == 1:
+        a[y] += 1
+        b[a[y]] += 1
+        if b[a[y]] == N:
+            diff = a[y]
+
+    if x == 2:
+        print(b[y + diff])
